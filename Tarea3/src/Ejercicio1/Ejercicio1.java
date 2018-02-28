d9456a354ff6ae982fa295dea2243385e9725ef9
Guillermo Crespo Aguayo <34092854+DSMan97@users.noreply.github.com>
2018-02-28 11:02:20 +0100

    Ejercicio1 Intentado

--- Tarea3/src/Ejercicio1/Ejercicio1.java
+++ Tarea3/src/Ejercicio1/Ejercicio1.java
@@ -1,10 +1,65 @@
 package Ejercicio1;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Ejercicio1 {
-
+static int sum=0;
+static int aux2=0;
+static char auxnum=0;
+static int i=0;
 	public static void main(String[] args) {
-		// TODO Ap�ndice de m�todo generado autom�ticamente
-
+		Scanner sc = new Scanner(System.in);
+		String nif = sc.next();
+		
+		Stack<Integer> digits = new Stack<Integer>();
+		// TODO Apéndice de método generado automáticamente
+		if (isNumeric(nif) == true) {// Validamos que los primeros 8 digitos sean numeros
+			int num = Integer.parseInt(nif);// Convertimos los 8 primeros digitos a numeros
+			
+			// suma de la descomposición
+			for (Integer elto : digits) {
+				sum+= elto;
+			}
+		}
+		System.out.println(sum);
 	}
+
+	private static boolean buscarnumero(String nif) {
+		  Stack<Character> digits = new Stack<Character>();
+		// TODO Apéndice de método generado automáticamente
+		 boolean flag = false;
+		    char[] array = nif.toCharArray();
+		    for (char caracter : array){
+		        if ( Character.isDigit(caracter) )
+		        	flag = true;
+		        if(flag==true) {
+		        	digits.add(caracter);
+		        	auxnum=digits.get(i);
+		        	aux2=auxnum;
+		        }
+		        
+		       
+		        System.out.println(aux2);
+		        i++;
+		    }   
+		 
+		    return flag;
+	}
+
+	public static boolean isNumeric(String nif) {
+		boolean resultado;
+
+		try {
+			Integer.parseInt(nif);
+			resultado = true;
+		} catch (NumberFormatException excepcion) {
+			buscarnumero(nif);
+			resultado = false;
+		}
+
+		return resultado;
+	}
+	
 
 }
